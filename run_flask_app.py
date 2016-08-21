a3f4a45422eeb59df35acff9fa248e1b9f3e00ae
Mike Tung <miketung2013@gmail.com>
2016-08-21 18:44:39 -0400

    update

--- run_flask_app.py
+++ run_flask_app.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from watchdog import watcher
 import os
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 def home_page():
     images = os.path.join(os.path.dirname(__file__), 'static/images/')
     img_fi = os.listdir(images)
-    img_fi = ['{}{}'.format(images, f) for f in img_fi]
+    img_fi = ['{}{}'.format(images, urllib.parse.quote(f)) for f in img_fi]
     return render_template('index.html', images=img_fi)
 
 if __name__ == '__main__':
