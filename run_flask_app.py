f554efdcda296c4d991c9895cc496bd2638e26a1
Mike Tung <miketung2013@gmail.com>
2016-09-04 20:35:09 -0400

    updated back

--- run_flask_app.py
+++ run_flask_app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 #import stuff
-from flask import Flask, render_template, request, redirect, url_for, abort
+from data_model.models import db
+from data_model.models import imdbInfo
+from flask import Flask
+from flask import render_template, request
+from flask import redirect, url_for, abort
 from flask_wtf import Form
-from watchdog import watcher
-import os
-import urllib.parse
-from data_model.models import db, imdbInfo
-from setting import DevelopmentConfig
 from fuzzywuzzy import fuzz
+import os
+from setting import DevelopmentConfig
+import urllib.parse
+from watchdog import watcher
 
 app = Flask(__name__)
 app.config.from_object('setting.Config')
@@ -29,11 +32,10 @@ def movies():
 def search():
     search_string = request.values['q'].strip()
     if search_string == '':
-        return redirect(url_for('shows', id='+'.join(k for k in imdbInfo.Title.distinct())))
-    #show_object = imdbInfo.query.filter(imdbInfo.Title.like("%{}%".format(request.values['q']))).first_or_404()
-    fuzzes = ((k, fuzz.partial_ratio(search_string, k)) for k in imdbInfo.Title.distinct())
+        return redirect(url_for('shows',
+            id='+'.join(k.Title for k in imdbInfo.query.all())))
+    fuzzes = ((k.Title, fuzz.partial_ratio(search_string, k.Title)) for k in imdbInfo.query.all())
     fuzzes = sorted(fuzzes, key=lambda x: x[1], reverse=True)
-    #print(fuzzes[:3])
     filter_fuzzes = (fuzz for fuzz in fuzzes if fuzz[1] >= 60)
     param_str = '+'.join(name[0] for name in filter_fuzzes)
     if param_str:
@@ -45,18 +47,10 @@ def search():
 def shows():
     shows = request.args.get('id').split('+')
     show_objects = (imdbInfo.query.filter_by(Title=k).first().TTid for k in shows)
-    #print(show_objects)
     if show_objects:
         return render_template('index.html', 
                 images=["../static/images/{}.jpg".format(k) for k in show_objects])
 
-# @app.route('/movies')
-# def home_page():
-#     images = os.path.join(os.path.dirname(__file__), 'static/images/')
-#     img_fi = os.listdir(images)
-#     img_fi = ['{}{}'.format(images, urllib.parse.quote(f)) for f in img_fi]
-#     #img_fi = ['{}{}'.format(images, f) for f in img_fi]
-#     return render_template('index.html', images=img_fi)
-
 if __name__ == '__main__':
-    app.run(port=DevelopmentConfig.PORT, debug=DevelopmentConfig.DEBUG)
+    app.run(port=DevelopmentConfig.PORT,
+            debug=DevelopmentConfig.DEBUG)
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from data_model.models import imdbInfo, db
 from data_model.models import Episodes2Show as ep2show
 import threading
@@ -8,44 +9,45 @@ doggo = Watcher()
 from time import clock
 
 def timer(func):
-	def wrapper(*args, **kwargs):
-		start = clock()
-		data = func(*args, **kwargs)
-		print("Time taken for {}: is {:.6f}".format(func.__name__, clock() - start))
-		return data
-	return wrapper
+    def wrapper(*args, **kwargs):
+        start = clock()
+        data = func(*args, **kwargs)
+        print("Time taken for {}: is {:.6f}".format(func.__name__, clock() - start))
+        return data
+    return wrapper
 
 @timer
 def threaded_get_all_episodes():
-	threads = []
-	programs = {}
-	#print(doggo.get_episodes(doggo.tracked_shows[0]))
-	for show in doggo.tracked_shows:
-		_thread = threading.Thread(target=doggo.episode_thread_worker, args=([show], programs))
-		threads.append(_thread)
-		_thread.start()
+    threads = []
+    programs = {}
+    #print(doggo.get_episodes(doggo.tracked_shows[0]))
+    for show in doggo.tracked_shows:
+        _thread = threading.Thread(
+                    target=doggo.episode_thread_worker, 
+                    args=([show], programs))
+        threads.append(_thread)
+        _thread.start()
 
-	for _thread in threads:
-		_thread.join()
-	return programs
+    for _thread in threads:
+        _thread.join()
+    return programs
 
 @timer
 def get_all_episodes():
-	programs = {}
-	for show in doggo.tracked_shows:
-		programs[show['title']] = doggo.get_episodes(show)
+    programs = {}
+    for show in doggo.tracked_shows:
+        programs[show['title']] = doggo.get_episodes(show)
 
-	return programs
+    return programs
 
 @timer
 def get_titles_from_watcher():
-	return doggo.get_show_titles()
+    return doggo.get_show_titles()
 
 @timer
 def get_titles_from_db():
-	return imdbInfo.Title.distinct()
+    return db.session.query(imdbInfo.Title.distinct()).all()
 
 a = get_titles_from_db()
 b = get_titles_from_watcher()
-#c = threaded_get_all_episodes()
-#d = get_all_episodes()
+print(a)
