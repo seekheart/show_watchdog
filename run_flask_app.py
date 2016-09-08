5bca63680255a98288474a93a48590cddf16a2da
Şamil NART <samil.nart@gmail.com>
2016-09-08 13:59:44 +0300

    Remove legacy route

--- run_flask_app.py
+++ run_flask_app.py
@@ -58,13 +58,5 @@ def shows():
         return render_template('index.html', 
                 images=["../static/images/{}.jpg".format(k) for k in show_objects])
 
-# @app.route('/movies')
-# def home_page():
-#     images = os.path.join(os.path.dirname(__file__), 'static/images/')
-#     img_fi = os.listdir(images)
-#     img_fi = ['{}{}'.format(images, urllib.parse.quote(f)) for f in img_fi]
-#     #img_fi = ['{}{}'.format(images, f) for f in img_fi]
-#     return render_template('index.html', images=img_fi)
-
 if __name__ == '__main__':
     app.run(port=DevelopmentConfig.PORT, debug=DevelopmentConfig.DEBUG)
